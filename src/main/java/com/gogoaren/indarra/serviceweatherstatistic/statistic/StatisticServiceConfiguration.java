a72b793fca0a6bfdd92f5525d8c769ccad023638
jablonski.pio <piotr.jablonski@bcf-software.com>
2021-12-16 15:33:54 +0100

    - clean code

--- src/main/java/com/gogoaren/indarra/serviceweatherstatistic/statistic/StatisticServiceConfiguration.java
+++ src/main/java/com/gogoaren/indarra/serviceweatherstatistic/statistic/StatisticServiceConfiguration.java
@@ -1,14 +1,10 @@
 package com.gogoaren.indarra.serviceweatherstatistic.statistic;
 
-import com.gogoaren.indarra.serviceweatherstatistic.model.Weather;
 import lombok.AllArgsConstructor;
-import org.codehaus.jackson.map.util.Comparators;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,7 +13,6 @@ import java.util.Map;
 public class StatisticServiceConfiguration {
 
     WeatherComparators weatherComparators;
-
 
     @Bean
     Map<StatisticType, Statistic> weatherStatisticsMap(final @Value("${statistic.record.size}") int size) {
@@ -29,21 +24,4 @@ public class StatisticServiceConfiguration {
 
         return statisticMap;
     }
-    /*  Instead of creating 3 beans, better is create Map of Statistics with key of Statistic Type */
-//    @Bean
-//    Statistic statisticTempMax() {
-//        return new Statistic(weatherComparators.getTemperatureAscending(), 10);
-//    }
-//
-//    @Bean
-//    Statistic statisticTempMin() {
-//        return new Statistic(weatherComparators.getTemperatureDescending(), 10);
-//    }
-//
-//    @Bean
-//    Statistic statisticWind() {
-//        return new Statistic(weatherComparators.getWind(), 10);
-//    }
-
-
 }
